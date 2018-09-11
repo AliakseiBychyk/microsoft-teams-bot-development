a2533e397abead9b5c00988faa7fcaba5dde3cf7
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-09-11 17:14:13 +0300

    config for heroku

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-const creds = require('./secret/config.json');
 const env = process.env;
 
 module.exports = {
-  APP_ID: env.APP_ID || creds.APP_ID,
-  APP_PASSWORD: env.APP_PASSWORD || creds.APP_PASSWORD,
+  APP_ID: env.APP_ID,
+  APP_PASSWORD: env.APP_PASSWORD,
   PORT: env.PORT || 3978,
 };
